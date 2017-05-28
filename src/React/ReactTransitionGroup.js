835c6f5be10f44b6a1a9a680b85da8e58b445663
Marcin Szamotulski <mszamot@gmail.com>
2017-05-28 17:21:54 -0600

    Initial commit

---
+++ src/React/ReactTransitionGroup.js
@@ -0,0 +1,5 @@
+"use strict"
+
+var rtg = require("react-transition-group");
+
+exports.cssTransitionGroup = rtg.CSSTransitionGroup
---
+++ src/React/ReactTransitionGroup.purs
@@ -0,0 +1,47 @@
+module React.ReactTranstionGroup where
+
+import Prelude (id)
+import React (ReactClass, ReactElement)
+import React.DOM.Props (Props)
+import Unsafe.Coerce (unsafeCoerce)
+
+type CSSTransitionGroupProps props =
+  { component :: ReactClass props
+  , transitionName ::
+    { enter :: String
+    , enterActive :: String
+    , leave :: String
+    , leaveActive :: String
+    , appear :: String
+    , appearActive :: String
+    }
+  , childFactory :: ReactElement -> ReactElement
+  , transitionAppear :: Boolean
+  , transitionAppearTimeout :: Int
+  , transitionLeave :: Boolean
+  , transitionLeaveTimeout :: Int
+  , transitionEnter :: Boolean
+  , transitionEnterTimeout :: Int
+  }
+
+defaultCSSTransitionGroupProps :: CSSTransitionGroupProps Props
+defaultCSSTransitionGroupProps =
+  { component: unsafeCoerce "span"
+  , transitionName:
+    { enter: "enter"
+    , enterActive: "enter-active"
+    , leave: "leave"
+    , leaveActive: "leave-active"
+    , appear: "appear"
+    , appearActive: "appear-active"
+    }
+  , childFactory: id
+  , transitionEnter: true
+  , transitionEnterTimeout: 300
+  , transitionLeave: true
+  , transitionLeaveTimeout: 300
+  , transitionAppear: true
+  , transitionAppearTimeout: 300
+  }
+
+foreign import cssTransitionGroup :: forall props. ReactClass (CSSTransitionGroupProps props)
