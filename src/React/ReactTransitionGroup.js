a58b8982a54a3e0aee1ad06e78de3d72b54bc4ba
Marcin Szamotulski <mszamot@gmail.com>
2017-06-03 22:27:46 +0200

    createTransitionReactClass in ffi module

--- src/React/ReactTransitionGroup.js
+++ src/React/ReactTransitionGroup.js
@@ -1,5 +1,63 @@
 "use strict"
 
+var React = require("react");
 var rtg = require("react-transition-group");
 
 exports.cssTransitionGroup = rtg.CSSTransitionGroup
+
+function createTransitionReactClass(spec) {
+  var result = {
+    displayName: spec.displayName,
+    render: function(){
+      return spec.render(this)()
+    },
+    getInitialState: function(){
+      return {
+        state: spec.getInitialState(this)()
+      };
+    },
+    componentWillMount: function(){
+      return spec.componentWillMount(this)()
+    },
+    componentDidMount: function(){
+      return spec.componentDidMount(this)()
+    },
+    componentWillReceiveProps: function(nextProps){
+      return spec.componentWillReceiveProps(this)(nextProps)()
+    },
+    shouldComponentUpdate: function(nextProps, nextState){
+      return spec.shouldComponentUpdate(this)(nextProps)(nextState.state)()
+    },
+    componentWillUpdate: function(nextProps, nextState){
+      return spec.componentWillUpdate(this)(nextProps)(nextState.state)()
+    },
+    componentDidUpdate: function(prevProps, prevState){
+      return spec.componentDidUpdate(this)(prevProps)(prevState.state)()
+    },
+    componentWillUnmount: function(){
+      return spec.componentWillUnmount(this)()
+    },
+    componentWillAppear: function(fn) {
+      return spec.componentWillAppear(this)(fn)()
+    },
+    componentDidAppear: function(fn) {
+      return spec.componentDidAppear(this)(fn)()
+    },
+    componentWillEnter: function(fn) {
+      return spec.componentWillEnter(this)(fn)()
+    },
+    componentDidEnter: function(fn) {
+      return spec.componentDidEnter(this)(fn)()
+    },
+    componentWillLeave: function(fn) {
+      return spec.componentWillLeave(this)(fn)()
+    },
+    componentDidLeave: function(fn) {
+      return spec.componentDidLeave(this)(fn)()
+    },
+  }
+
+  return React.createClass(result)
+}
+
+exports.createTransitionReactClass = createTransitionReactClass
